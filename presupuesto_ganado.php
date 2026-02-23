8c7fb4246e583475146aa6a8b188d7874abdffd2
unknown <jazmin.gaby.iq@gmail.com>
2026-02-23 15:36:41 -0400

    Presupuesto ganado

--- presupuesto_ganado.php
+++ presupuesto_ganado.php
@@ -6,18 +6,11 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 if ($data) {
     try {
-<<<<<<< HEAD
         $stmt = $conn->prepare("UPDATE proyecto SET estado_id = ?, monto_adjudicado = ?, fecha_cierre = ? WHERE id_proyecto = ?");
         $result = $stmt->execute([
             $data['estado_id'],
             $data['monto_adjudicado'],
             $data['fecha_cierre'],
-=======
-        $stmt = $conn->prepare("UPDATE proyecto SET estado_id = ?, monto_adjudicado = ? WHERE id_proyecto = ?");
-        $result = $stmt->execute([
-            $data['estado_id'],
-            $data['monto_adjudicado'],
->>>>>>> 0850aed84fb37fc17115e59d49ab3c61010c1c17
             $data['id_proyecto']
         ]);
 
